af3d5b9f95ca554348d7a89f0cc44d294379d004
qiaoooo <muqiao414@gmail.com>
2023-03-25 22:32:15 -0700

    add dotenv to production configure file

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -20,6 +21,8 @@ module.exports = {
     ],
   },
   plugins: [
+    new Dotenv(),
+
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
